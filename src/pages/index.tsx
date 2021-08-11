61f25d087cf9ef5e0e57ed5282fa4f2cb1ba6215
matheus.martinez <matheus.martinez@uppertools.inf.br>
2021-08-11 08:01:43 -0300

    Finalização estilização página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,35 +32,83 @@ export default function Home() {
     <>
       <main className={styles.contentContainer}>
         <section className={styles.posts}>
-          <img src="/images/Logo.svg" alt="Logo"></img>
+          <Link href="#">
+            <a href="#">
+              <img src="/images/Logo.svg" alt="logo"></img>
+            </a>
+          </Link>
           <Link href="#">
             <a href="#">
               <h1>Como utilizar Hooks</h1>
               <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
               <div className={styles.bottomPost}>
-              <img src="/images/calendar.svg" alt="Calendário"></img>
-              <time>15 Mar 2021</time>
-              <img src="/images/user.svg" alt="Usuário"></img>
-              <span>Matheus Martinez</span>
+                <img src="/images/calendar.svg" alt="Calendário"></img>
+                <time>15 Mar 2021</time>
+                <img src="/images/user.svg" alt="Usuário"></img>
+                <span>Matheus Martinez</span>
               </div>
-            </a>
-          </Link>
-          {/* <Link href="#">
-            <a href="#">
-              <strong>Como utilizar Hooks</strong>
-              <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
-              <time>15 Mar 2021</time>
-              <span>Matheus Martinez</span>
             </a>
           </Link>
           <Link href="#">
             <a href="#">
-              <strong>Como utilizar Hooks</strong>
-              <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
-              <time>15 Mar 2021</time>
-              <span>Matheus Martinez</span>
+              <h1>Criando um app CRA do zero</h1>
+              <h2>
+                Tudo sobre como criar sua primeira aplicação utilizando Create
+                React App.
+              </h2>
+              <div className={styles.bottomPost}>
+                <img src="/images/calendar.svg" alt="Calendário"></img>
+                <time>15 Mar 2021</time>
+                <img src="/images/user.svg" alt="Usuário"></img>
+                <span>Matheus Martinez</span>
+              </div>
             </a>
-          </Link> */}
+          </Link>
+          <Link href="#">
+            <a href="#">
+              <h1>Como utilizar Hooks</h1>
+              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+              <div className={styles.bottomPost}>
+                <img src="/images/calendar.svg" alt="Calendário"></img>
+                <time>15 Mar 2021</time>
+                <img src="/images/user.svg" alt="Usuário"></img>
+                <span>Matheus Martinez</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="#">
+            <a href="#">
+              <h1>Criando um app CRA do zero</h1>
+              <h2>
+                Tudo sobre como criar sua primeira aplicação utilizando Create
+                React App.
+              </h2>
+              <div className={styles.bottomPost}>
+                <img src="/images/calendar.svg" alt="Calendário"></img>
+                <time>15 Mar 2021</time>
+                <img src="/images/user.svg" alt="Usuário"></img>
+                <span>Matheus Martinez</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="#">
+
+            <a href="#">
+              <h1>Como utilizar Hooks</h1>
+              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+              <div className={styles.bottomPost}>
+                <img src="/images/calendar.svg" alt="Calendário"></img>
+                <time>15 Mar 2021</time>
+                <img src="/images/user.svg" alt="Usuário"></img>
+                <span>Matheus Martinez</span>
+              </div>
+            </a>
+          </Link>
+          <Link href="#">
+          <a href="#">
+            <span>Carregar mais posts</span>
+          </a>
+          </Link>
         </section>
       </main>
     </>
