5680fa5cb66903fca60544cf993209858e95a51e
matheus.martinez <matheus.martinez@uppertools.inf.br>
2021-08-10 22:15:58 -0300

    Estilização página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
+import { Head } from 'next/document';
+import React from 'react';
 
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Link from 'next/link';
 
 interface Post {
   uid?: string;
@@ -24,9 +27,45 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={styles.contentContainer}>
+        <section className={styles.posts}>
+          <img src="/images/Logo.svg" alt="Logo"></img>
+          <Link href="#">
+            <a href="#">
+              <h1>Como utilizar Hooks</h1>
+              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+              <div className={styles.bottomPost}>
+              <img src="/images/calendar.svg" alt="Calendário"></img>
+              <time>15 Mar 2021</time>
+              <img src="/images/user.svg" alt="Usuário"></img>
+              <span>Matheus Martinez</span>
+              </div>
+            </a>
+          </Link>
+          {/* <Link href="#">
+            <a href="#">
+              <strong>Como utilizar Hooks</strong>
+              <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
+              <time>15 Mar 2021</time>
+              <span>Matheus Martinez</span>
+            </a>
+          </Link>
+          <Link href="#">
+            <a href="#">
+              <strong>Como utilizar Hooks</strong>
+              <h3>Pensando em sincronização em vez de ciclos de vida.</h3>
+              <time>15 Mar 2021</time>
+              <span>Matheus Martinez</span>
+            </a>
+          </Link> */}
+        </section>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
