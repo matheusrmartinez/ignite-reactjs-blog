ca245d4a7f458bfab28ec7413c403aedc52a8e33
matheus.martinez <matheus.martinez@uppertools.inf.br>
2021-08-12 22:48:45 -0300

    Consulta para recuperar página com todos os posts.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
+import Prismic from '@prismicio/client';
 import styles from './home.module.scss';
 import Link from 'next/link';
 
@@ -27,97 +28,52 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({postsPagination}: HomeProps) {
   return (
     <>
       <main className={styles.contentContainer}>
         <section className={styles.posts}>
-          <Link href="#">
+        <Link href="#">
             <a href="#">
               <img src="/images/Logo.svg" alt="logo"></img>
             </a>
           </Link>
-          <Link href="#">
+          {postsPagination.results.map(posts => {
+            return (
+              <Link href="#">
+              <a href="#">
+                <h1>{posts.data.title}</h1>
+                <h2>{posts.data.subtitle}</h2>
+                <div className={styles.bottomPost}>
+                  <img src="/images/calendar.svg" alt="Calendário"></img>
+                  <time>{posts.first_publication_date}</time>
+                  <img src="/images/user.svg" alt="Usuário"></img>
+                  <span>{posts.data.author}</span>
+                </div>
+              </a>
+            </Link>
+            )
+          })}
             <a href="#">
-              <h1>Como utilizar Hooks</h1>
-              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
-              <div className={styles.bottomPost}>
-                <img src="/images/calendar.svg" alt="Calendário"></img>
-                <time>15 Mar 2021</time>
-                <img src="/images/user.svg" alt="Usuário"></img>
-                <span>Matheus Martinez</span>
-              </div>
+              <span>Carregar mais posts</span>
             </a>
-          </Link>
-          <Link href="#">
-            <a href="#">
-              <h1>Criando um app CRA do zero</h1>
-              <h2>
-                Tudo sobre como criar sua primeira aplicação utilizando Create
-                React App.
-              </h2>
-              <div className={styles.bottomPost}>
-                <img src="/images/calendar.svg" alt="Calendário"></img>
-                <time>15 Mar 2021</time>
-                <img src="/images/user.svg" alt="Usuário"></img>
-                <span>Matheus Martinez</span>
-              </div>
-            </a>
-          </Link>
-          <Link href="#">
-            <a href="#">
-              <h1>Como utilizar Hooks</h1>
-              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
-              <div className={styles.bottomPost}>
-                <img src="/images/calendar.svg" alt="Calendário"></img>
-                <time>15 Mar 2021</time>
-                <img src="/images/user.svg" alt="Usuário"></img>
-                <span>Matheus Martinez</span>
-              </div>
-            </a>
-          </Link>
-          <Link href="#">
-            <a href="#">
-              <h1>Criando um app CRA do zero</h1>
-              <h2>
-                Tudo sobre como criar sua primeira aplicação utilizando Create
-                React App.
-              </h2>
-              <div className={styles.bottomPost}>
-                <img src="/images/calendar.svg" alt="Calendário"></img>
-                <time>15 Mar 2021</time>
-                <img src="/images/user.svg" alt="Usuário"></img>
-                <span>Matheus Martinez</span>
-              </div>
-            </a>
-          </Link>
-          <Link href="#">
-
-            <a href="#">
-              <h1>Como utilizar Hooks</h1>
-              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
-              <div className={styles.bottomPost}>
-                <img src="/images/calendar.svg" alt="Calendário"></img>
-                <time>15 Mar 2021</time>
-                <img src="/images/user.svg" alt="Usuário"></img>
-                <span>Matheus Martinez</span>
-              </div>
-            </a>
-          </Link>
-          <Link href="#">
-          <a href="#">
-            <span>Carregar mais posts</span>
-          </a>
-          </Link>
         </section>
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsPagination = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100,
+    }
+  );
 
-//   // TODO
-// };
+  return {
+    props: { postsPagination },
+  };
+};
